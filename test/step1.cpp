08c0604b0d545c2170e53cda507bb232cde883d6
Brian Granzow <brian.granzow@gmail.com>
2017-03-22 11:09:57 -0400

    test: finish up step1

--- test/step1.cpp
+++ test/step1.cpp
@@ -1,20 +1,20 @@
+/** [Step1 all] */
 #include <apf.h>
 #include <apfBox.h>
 #include <goal_control.hpp>
 
 int main() {
-
-  /* [Step1 initialize] */
+  /** [Step1 initialize] */
   goal::initialize();
-  /* [Step1 initialize] */
+  /** [Step1 initialize] */
 
-  /** [Step1 finalize] */
+  /** [Step1 print] */
   goal::print("Goal: Step 1");
-  /** [Step1 finalize] */
+  /** [Step1 print] */
   
   /** [Step1 create] */
   auto mesh = apf::makeMdsBox(20, 20, 0, 1.0, 1.0, 0.0, true);
-  /** [Step1 createmesh] */
+  /** [Step1 create] */
 
   /** [Step1 verify] */
   mesh->verify();
@@ -27,6 +27,5 @@ int main() {
   /** [Step1 finalize] */
   goal::finalize();
   /** [Step1 finalize] */
-
-  return 0;
 }
+/** [Step1 all] */
