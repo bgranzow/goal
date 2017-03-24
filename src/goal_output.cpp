76927c38fbd5563241175c160e0f5488e6cef83f
Brian Granzow <brian.granzow@gmail.com>
2017-03-24 17:16:51 -0400

    output

---
+++ src/goal_output.cpp
@@ -0,0 +1,92 @@
+#include <PCU.h>
+#include <apf.h>
+#include <apfMesh2.h>
+#include <apfShape.h>
+#include <Teuchos_ParameterList.hpp>
+
+#include "goal_discretization.hpp"
+#include "goal_output.hpp"
+
+namespace goal {
+
+using Teuchos::rcp;
+
+static RCP<ParameterList> get_valid_params() {
+  using Teuchos::Array;
+  Array<std::string> dummy(0);
+  auto p = rcp(new ParameterList);
+  p->set<std::string>("out file", "");
+  p->set<int>("interval", 1);
+  p->set<bool>("turn off", false);
+  p->set<Array<std::string> >("interpolate", dummy);
+  return p;
+}
+
+static void validate_params(RCP<const ParameterList> p) {
+  assert(p->isType<std::string>("out file"));
+  p->validateParameters(*get_valid_params(), 0);
+}
+
+static void write_initial_pvd(std::string const& name, int& pos) {
+  if (!PCU_Comm_Self()) {
+    auto pvd = name + ".pvd";
+    std::fstream pvdf;
+    pvdf.open(pvd.c_str(), std::ios::out);
+    pvdf << "<VTKFile type=\"Collection\" version=\"0.1\">" << std::endl;
+    pvdf << "  <Collection>" << std::endl;
+    pos = pvdf.tellp();
+    pvdf << "  </Collection>" << std::endl;
+    pvdf << "</VTKFile>" << std::endl;
+    pvdf.close();
+  }
+}
+
+Output::Output(RCP<const ParameterList> p, RCP<Discretization> d) {
+  params = p;
+  disc = d;
+  validate_params(params);
+  turn_off = false;
+  interval = 1;
+  pos = 0;
+  index = 0;
+  name = params->get<std::string>("out file");
+  if (params->isParameter("turn off")) turn_off = params->get<bool>("turn off");
+  if (params->isParameter("interval")) interval = params->get<int>("interval");
+  if (!turn_off) write_initial_pvd(name, pos);
+}
+
+static void update_pvd(
+    std::string const& name, std::string const& vtu, int& pos, const double t) {
+  if (!PCU_Comm_Self()) {
+    std::string pvd = name + ".pvd";
+    std::fstream pvdf;
+    pvdf.open(pvd.c_str(), std::ios::out | std::ios::in);
+    pvdf.seekp(pos);
+    pvdf << "    <DataSet timestep=\"" << t << "\" group=\"\" ";
+    pvdf << "part=\"0\" file=\"" << vtu << "/" << vtu;
+    pvdf << ".pvtu\"/>" << std::endl;
+    pos = pvdf.tellp();
+    pvdf << "  </Collection>" << std::endl;
+    pvdf << "</VTKFile>" << std::endl;
+    pvdf.close();
+  }
+}
+
+void Output::write_vtk(const double t) {
+  auto m = disc->get_apf_mesh();
+  std::ostringstream oss;
+  oss << name << "_" << index;
+  std::string vtu = oss.str();
+  update_pvd(name, vtu, pos, t);
+  apf::writeVtkFiles(vtu.c_str(), m);
+  ++index;
+}
+
+void Output::write(const double t) {
+  if (turn_off) return;
+  static int my_out_interval = 0;
+  if (my_out_interval++ % interval) return;
+  write_vtk(t);
+}
+
+}  // namespace goal
---
+++ src/goal_output.hpp
@@ -0,0 +1,62 @@
+#ifndef GOAL_OUTPUT_HPP
+#define GOAL_OUTPUT_HPP
+
+/** \file goal_output.hpp */
+
+#include <Teuchos_RCP.hpp>
+
+namespace Teuchos {
+class ParameterList;
+};
+
+namespace goal {
+
+/** \cond */
+using Teuchos::RCP;
+
+class Discretization;
+/** \endcond */
+
+/** \brief A class to manage visualization output over multiple steps.
+  * \details The output object exists to provide an easy interface to manage
+  * dumping out visualization output data in VTK file format over multiple
+  * time or load steps, including options to write output only at specific
+  * intervals and to turn off output completely. */
+class Output {
+ public:
+  /** brief The output constructor.
+    * \param p A parameterlist to describe the output behavior
+    *
+    * Parameter Name  | Parameter Type
+    * --------------- | --------------
+    * out file        | std::string
+    * interval        | int
+    * turn off        | bool
+    *
+    * parameter descriptions:
+    * - out file, the base name to dump the visualization output.
+    * - interval, dump the ouptut only at every \f$ i^{th} \f$ specified
+    * interval for which \ref Output::write is called.
+    * - turn off, completely turn off dumping output to file.
+    *
+    * \param d The relevant \ref goal::Discretization. */
+  Output(RCP<const ParameterList> p, RCP<Discretization> d);
+
+  /** \brief Write the visualization output.
+    * \param t The time associated with the current output. */
+  void write(const double t);
+
+ private:
+  void write_vtk(const double t);
+  RCP<const ParameterList> params;
+  RCP<Discretization> disc;
+  bool turn_off;
+  int interval;
+  std::string name;
+  int pos;
+  int index;
+};
+
+} // namesapce goal
+
+#endif
