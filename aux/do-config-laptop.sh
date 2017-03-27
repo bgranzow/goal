8f1076e230fdd06bded3e5aa4997b738563896c7
Brian Granzow <brian.granzow@gmail.com>
2017-03-27 09:20:47 -0400

    aux: laptop config for linux

--- aux/do-config-laptop.sh
+++ aux/do-config-laptop.sh
@@ -1,7 +1,7 @@
 # Modify these paths for your system
-SCOREC_DIR=/Users/bng/codes/core/install-opt
-TRILINOS_DIR=/Users/bng/codes/trilinos/install-opt
-GOAL_INSTALL=/Users/bng/codes/goal/install-opt
+SCOREC_DIR=/home/bng/codes/core/install
+TRILINOS_DIR=/home/bng/codes/trilinos/install
+GOAL_INSTALL=/home/bng/codes/goal/install
 
 cmake \
  -D CMAKE_CXX_COMPILER="mpicxx" \
@@ -10,5 +10,5 @@ cmake \
  -D Scorec_PREFIX=$SCOREC_DIR \
  -D Trilinos_PREFIX=$TRILINOS_DIR \
  -D GOAL_FAD_SIZE=40 \
- -D Goal_CXX_FLAGS="-std=c++11 -g -O2 -Werror -Wall -Wextra -ferror-limit=2" \
+ -D GOAL_ENABLE_ALL_APPS=ON \
 ..
