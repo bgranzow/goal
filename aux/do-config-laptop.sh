44aaed7199a39e3214f112e206ac65f1ec31729d
Brian Granzow <brian.granzow@gmail.com>
2017-03-27 20:21:43 -0400

    cmake: Goal_DATA var

--- aux/do-config-laptop.sh
+++ aux/do-config-laptop.sh
@@ -2,6 +2,7 @@
 SCOREC_DIR=/home/bng/codes/core/install
 TRILINOS_DIR=/home/bng/codes/trilinos/install
 GOAL_INSTALL=/home/bng/codes/goal/install
+GOAL_DATA_DIR=/home/bng/codes/goal-data
 
 cmake \
  -D CMAKE_CXX_COMPILER="mpicxx" \
@@ -9,6 +10,7 @@ cmake \
  -D BUILD_TESTING=ON \
  -D Scorec_PREFIX=$SCOREC_DIR \
  -D Trilinos_PREFIX=$TRILINOS_DIR \
+ -D Goal_DATA=$GOAL_DATA_DIR \
  -D Goal_FAD_SIZE=40 \
  -D Goal_ENABLE_ALL_APPS=ON \
 ..
