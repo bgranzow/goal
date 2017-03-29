165179528cd6807b11a29a3710f4fcbda3f7be85
Brian Granzow <brian.granzow@gmail.com>
2017-03-29 14:49:45 -0400

    update config scripts

--- aux/do-config-laptop.sh
+++ aux/do-config-laptop.sh
@@ -1,16 +1,19 @@
+#!/bin/bash -ex
+
 # Modify these paths for your system
 SCOREC_DIR=/home/bng/codes/core/install
 TRILINOS_DIR=/home/bng/codes/trilinos/install
-GOAL_INSTALL=/home/bng/codes/goal/install
+GOAL_SRC_DIR=/home/bng/codes/goal
+GOAL_INSTALL_DIR=/home/bng/codes/goal/install
 GOAL_DATA_DIR=/home/bng/codes/goal-data
 
-cmake \
- -D CMAKE_CXX_COMPILER="mpicxx" \
- -D CMAKE_INSTALL_PREFIX=$GOAL_INSTALL \
- -D BUILD_TESTING=ON \
- -D Scorec_PREFIX=$SCOREC_DIR \
- -D Trilinos_PREFIX=$TRILINOS_DIR \
- -D Goal_DATA=$GOAL_DATA_DIR \
- -D Goal_FAD_SIZE=40 \
- -D Goal_ENABLE_ALL_APPS=ON \
-..
+cmake $GOAL_SRC_DIR \
+-DCMAKE_CXX_COMPILER="mpicxx" \
+-DCMAKE_INSTALL_PREFIX=$GOAL_INSTALL_DIR \
+-DBUILD_TESTING=ON \
+-DScorec_PREFIX=$SCOREC_DIR \
+-DTrilinos_PREFIX=$TRILINOS_DIR \
+-DGoal_DATA=$GOAL_DATA_DIR \
+-DGoal_FAD_SIZE=40 \
+-DGoal_ENABLE_ALL_APPS=ON \
+2>&1 | tee config_log
