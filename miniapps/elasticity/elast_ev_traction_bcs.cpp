c6c9dcd9021bfee6a0e77b0064513111bd30bf00
Brian Granzow <brian.granzow@gmail.com>
2017-04-04 12:33:52 -0400

    issue #20 - traction bcs in elasticity

---
+++ miniapps/elasticity/elast_ev_traction_bcs.cpp
@@ -0,0 +1,121 @@
+#include <apf.h>
+#include <apfNumbering.h>
+#include <apfShape.h>
+#include <goal_control.hpp>
+#include <goal_discretization.hpp>
+#include <goal_field.hpp>
+#include <goal_indexer.hpp>
+#include <goal_solution_info.hpp>
+#include <goal_traits.hpp>
+#include <goal_workset.hpp>
+#include <Phalanx_DataLayout_MDALayout.hpp>
+
+#include "elast_ev_traction_bcs.hpp"
+
+namespace elast {
+
+using Teuchos::rcp;
+using Teuchos::rcpFromRef;
+using Teuchos::ParameterEntry;
+using Teuchos::getValue;
+using Teuchos::Array;
+
+template <typename EVALT, typename TRAITS>
+void TractionBCs<EVALT, TRAITS>::validate_params() {
+  for (auto it = params->begin(); it != params->end(); ++it) {
+    auto entry = params->entry(it);
+    auto a = getValue<Array<std::string> >(entry);
+    assert(a.size() == num_dims + 1);
+    auto set = a[0];
+    disc->get_sides(set);
+  }
+}
+
+template <typename EVALT, typename TRAITS>
+TractionBCs<EVALT, TRAITS>::TractionBCs(
+    RCP<goal::Indexer> i, RCP<const ParameterList> p) {
+  params = p;
+  indexer = i;
+  disc = indexer->get_discretization();
+  num_dims = indexer->get_field(0)->get_num_dims();
+  validate_params();
+  auto name = "Traction BCs";
+  PHX::Tag<ScalarT> op(name, rcp(new PHX::MDALayout<Dummy>(0)));
+  this->addEvaluatedField(op);
+  this->setName(name);
+}
+
+template <typename EVALT, typename TRAITS>
+void TractionBCs<EVALT, TRAITS>::postRegistrationSetup(
+    SetupData d, PHX::FieldManager<TRAITS>& fm) {
+  (void)d;
+  (void)fm;
+}
+
+template <typename EVALT, typename TRAITS>
+void TractionBCs<EVALT, TRAITS>::preEvaluate(PreEvalData i) {
+  info = rcpFromRef(i);
+  assert(info->ghost->R != Teuchos::null);
+}
+
+template <typename EVALT, typename TRAITS>
+void TractionBCs<EVALT, TRAITS>::apply_bc(
+    EvalData workset, Teuchos::Array<std::string> const& a) {
+  int idx = 0;
+  auto t = workset.t_current;
+  auto R = info->ghost->R;
+  auto set = a[0];
+  auto sides = disc->get_sides(set);
+  auto u = indexer->get_field(idx);
+  auto q_degree = u->get_q_degree();
+  auto basis = u->get_apf_basis();
+  auto mesh = indexer->get_apf_mesh();
+  auto numbering = indexer->get_ghost_apf_numbering(idx);
+  apf::Vector3 xi(0, 0, 0);
+  apf::Vector3 x(0, 0, 0);
+  apf::Vector3 traction(0, 0, 0);
+  apf::NewArray<int> numbers;
+  apf::NewArray<double> BF;
+
+  for (size_t i = 0; i < sides.size(); ++i) {
+    auto f = sides[i];
+    auto me = apf::createMeshElement(mesh, f);
+    apf::getElementNumbers(numbering, f, numbers);
+    int num_ips = apf::countIntPoints(me, q_degree);
+    auto es = basis->getEntityShape(mesh->getType(f));
+    int num_nodes = es->countNodes();
+    for (int ip = 0; ip < num_ips; ++ip) {
+      apf::getIntPoint(me, q_degree, ip, xi);
+      apf::mapLocalToGlobal(me, xi, x);
+      apf::getBF(basis, me, xi, BF);
+      double w = apf::getIntWeight(me, q_degree, ip);
+      double dv = apf::getDV(me, xi);
+      for (int i = 0; i < num_dims; ++i) {
+        traction[i] = goal::eval(a[i + 1], x[0], x[1], x[2], t);
+      }
+      int dof = 0;
+      for (int node = 0; node < num_nodes; ++node) {
+        for (int dim = 0; dim < num_dims; ++dim) {
+          goal::LO row = numbers[dof++];
+          double val = -BF[node] * traction[dim] * w * dv;
+          R->sumIntoLocalValue(row, val);
+        }
+      }
+    }
+    apf::destroyMeshElement(me);
+  }
+}
+
+template <typename EVALT, typename TRAITS>
+void TractionBCs<EVALT, TRAITS>::evaluateFields(EvalData workset) {
+  for (auto i = params->begin(); i != params->end(); ++i) {
+    ParameterEntry const& entry = params->entry(i);
+    Array<std::string> a = getValue<Array<std::string> >(entry);
+    apply_bc(workset, a);
+  }
+}
+
+template class TractionBCs<goal::Traits::Residual, goal::Traits>;
+template class TractionBCs<goal::Traits::Jacobian, goal::Traits>;
+
+}  // namespace elast
---
+++ miniapps/elasticity/elast_ev_traction_bcs.hpp
@@ -0,0 +1,89 @@
+#ifndef GOAL_EV_TRACTION_BCS_HPP
+#define GOAL_EV_TRACTION_BCS_HPP
+
+#include <Phalanx_Evaluator_WithBaseImpl.hpp>
+#include <Phalanx_Evaluator_Derived.hpp>
+
+#include "goal_dimension.hpp"
+
+/** \cond */
+namespace goal {
+class Indexer;
+class Discretization;
+class SolutionInfo;
+}
+/** \endcond */
+
+namespace elast {
+
+/** \cond */
+using Teuchos::RCP;
+using Teuchos::ParameterList;
+/** \endcond */
+
+/** \brief An evaluator for traction boundary conditions.
+  * \details Modifies the residual vector to appropriately account for
+  * contributions of traction defined over side sets. That is, given a user
+  * specified traction \f$ t \f$, the right hand side will be modified by
+  * the integrals over mesh sides (edges in 2D and faces in 3D) as:
+  * \f[
+  * ( t, w )_{\Gamma}
+  * \f]
+  * where \f$ w \f$ denotes the vector-valued weighting function.
+  *
+  * dependent fields  | data layout
+  * ----------------  | -----------
+  * none              | N/A
+  *
+  * evaluated fields  | data layout
+  * ----------------  | -----------
+  * op                | Dummy
+  */
+template <typename EVALT, typename TRAITS>
+class TractionBCs : public PHX::EvaluatorWithBaseImpl<TRAITS>,
+                    public PHX::EvaluatorDerived<EVALT, TRAITS> {
+ public:
+  /** \cond */
+  typedef typename TRAITS::SetupData SetupData;
+  typedef typename TRAITS::PreEvalData PreEvalData;
+  typedef typename TRAITS::PostEvalData PostEvalData;
+  typedef typename TRAITS::EvalData EvalData;
+  typedef typename EVALT::ScalarT ScalarT;
+  using Elem = goal::Elem;
+  using Node = goal::Node;
+  using Dim = goal::Dim;
+  using IP = goal::IP;
+  using Dummy = goal::Dummy;
+  /** \endcond */
+
+  /** \brief Construct the traction BCs evaluator.
+    * \param i The relevant \ref goal::Indexer structure.
+    * \param p The traction boundary condition parameterlist, containing
+    * entries of type Teuchos::Array<std::string> of the form:
+    *  - [ side set name, t_x, t_y, t_z ]. */
+  TractionBCs(RCP<goal::Indexer> i, RCP<const ParameterList> p);
+
+  /** \brief Finalize the field manager registration. */
+  void postRegistrationSetup(SetupData d, PHX::FieldManager<TRAITS>& fm);
+
+  /** \brief Grab the linear algebra data structures.
+    * \param info The PreEvalData structure (\ref goal::SolutionInfo). */
+  void preEvaluate(PreEvalData info);
+
+  /** \brief Fill in the local multidimensional arrays. */
+  void evaluateFields(EvalData workset);
+
+ private:
+  void validate_params();
+  void apply_bc(EvalData workset, Teuchos::Array<std::string> const& a);
+
+  RCP<const ParameterList> params;
+  RCP<goal::Indexer> indexer;
+  RCP<goal::Discretization> disc;
+  RCP<goal::SolutionInfo> info;
+  int num_dims;
+};
+
+}  // namespace elast
+
+#endif
