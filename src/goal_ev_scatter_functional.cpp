1a0bb7b5bd09d277abff2bbc184123e80f8112e2
Brian Granzow <brian.granzow@gmail.com>
2017-04-05 09:43:51 -0400

    issue #23

---
+++ src/goal_ev_scatter_functional.cpp
@@ -0,0 +1,61 @@
+#include <Phalanx_DataLayout_MDALayout.hpp>
+
+#include "goal_ev_scatter_functional.hpp"
+#include "goal_field.hpp"
+#include "goal_indexer.hpp"
+#include "goal_solution_info.hpp"
+#include "goal_workset.hpp"
+
+namespace goal {
+
+using Teuchos::rcp;
+using Teuchos::rcpFromRef;
+
+template <typename TRAITS>
+ScatterFunctional<goal::Traits::Jacobian, TRAITS>::ScatterFunctional(
+    RCP<Field> f, RCP<Indexer> i, std::string const& qoi) {
+  field = f;
+  indexer = i;
+  num_dofs = indexer->get_num_total_elem_dofs();
+  functional =
+      PHX::MDField<const ScalarT, Elem>(qoi, field->get_elem_scalar_dl());
+  auto name = "Scatter Functional";
+  PHX::Tag<ScalarT> op(name, rcp(new PHX::MDALayout<Dummy>(0)));
+  this->addDependentField(functional);
+  this->addEvaluatedField(op);
+  this->setName(name);
+}
+
+template <typename TRAITS>
+void ScatterFunctional<goal::Traits::Jacobian, TRAITS>::postRegistrationSetup(
+    SetupData d, PHX::FieldManager<TRAITS>& fm) {
+  this->utils.setFieldData(functional, fm);
+  (void)d;
+}
+
+template <typename TRAITS>
+void ScatterFunctional<goal::Traits::Jacobian, TRAITS>::preEvaluate(
+    PreEvalData i) {
+  info = rcpFromRef(i);
+  assert(info->ghost->dJdu != Teuchos::null);
+}
+
+template <typename TRAITS>
+void ScatterFunctional<goal::Traits::Jacobian, TRAITS>::evaluateFields(
+    EvalData workset) {
+  auto dJdu = info->ghost->dJdu;
+  std::vector<LO> rows;
+  for (int elem = 0; elem < workset.size; ++elem) {
+    auto e = workset.entities[elem];
+    indexer->get_ghost_lids(e, rows);
+    for (int dof = 0; dof < num_dofs; ++dof) {
+      LO row = rows[dof];
+      auto val = functional(elem).fastAccessDx(dof);
+      dJdu->sumIntoLocalValue(row, val);
+    }
+  }
+}
+
+template class ScatterFunctional<goal::Traits::Jacobian, goal::Traits>;
+
+}  // namespace goal
---
+++ src/goal_ev_scatter_functional.hpp
@@ -0,0 +1,81 @@
+#ifndef GOAL_EV_SCATTER_FUNCTIONAL
+#define GOAL_EV_SCATTER_FUNCTIONAL
+
+/** \file goal_ev_scatter_functional.hpp */
+
+#include <Phalanx_Evaluator_Derived.hpp>
+#include <Phalanx_Evaluator_WithBaseImpl.hpp>
+
+#include "goal_dimension.hpp"
+#include "goal_traits.hpp"
+
+namespace goal {
+
+/** \cond */
+using Teuchos::RCP;
+
+class Field;
+class Indexer;
+
+template <typename EVALT, typename TRAITS>
+class ScatterFunctional;
+/** \endcond */
+
+/** \brief Fill in the functional derivative vector.
+  * \details This evaluator will scatter local contributinos to the functional
+  * derivative vector \f$ \frac{\partial J}{\partial u} \f$ based on element
+  * level contributions to the function \f$ J(u) \f$ of the form:
+  * \f[
+  * J^e(u) = \int_{\Omega^e} j(u) \text{d} \Omega.
+  * \f]
+  * Notice that this class is only instantiated for the
+  * \ref goal::Traits::Jacobian specialization.
+  *
+  * dependent fields  | data layout
+  * ----------------  | -----------
+  * functional        | (Elem)
+  *
+  * evaluated fields  | data layout
+  * ----------------  | -----------
+  * op                | (Dummy)
+  */
+template <typename TRAITS>
+class ScatterFunctional<goal::Traits::Jacobian, TRAITS>
+    : public PHX::EvaluatorWithBaseImpl<TRAITS>,
+      public PHX::EvaluatorDerived<goal::Traits::Jacobian, TRAITS> {
+ public:
+  /** \cond */
+  typedef typename TRAITS::SetupData SetupData;
+  typedef typename TRAITS::PreEvalData PreEvalData;
+  typedef typename TRAITS::PostEvalData PostEvalData;
+  typedef typename TRAITS::EvalData EvalData;
+  typedef typename goal::Traits::Jacobian::ScalarT ScalarT;
+  /** \endcond */
+
+  /** \brief Construct the evaluator.
+    * \param f The relevant DOF \ref goal::Field.
+    * \param i The relevant \ref goal::Indexer object.
+    * \param qoi The name of the functional to be scattered. */
+  ScatterFunctional(RCP<Field> f, RCP<Indexer> i, std::string const& qoi);
+
+  /** \brief Finalize the field manager registration. */
+  void postRegistrationSetup(SetupData d, PHX::FieldManager<TRAITS>& fm);
+
+  /** \brief Grab the linear algebra data structures.
+    * \param info The PreEvalData structure (\ref goal::SolutionInfo). */
+  void preEvaluate(PreEvalData info);
+
+  /** \brief Sum contributions to the functional derivative vector. */
+  void evaluateFields(EvalData workset);
+
+ private:
+  int num_dofs;
+  RCP<Field> field;
+  RCP<Indexer> indexer;
+  RCP<SolutionInfo> info;
+  PHX::MDField<const ScalarT, Elem> functional;
+};
+
+}  // namespace goal
+
+#endif
