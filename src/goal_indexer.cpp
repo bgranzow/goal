e19632ff01926a2e4b8eb5ad91cdbd74c09b07cb
Brian Granzow <brian.granzow@gmail.com>
2017-05-14 19:50:09 -0400

    abstract indexer

---
+++ src/goal_indexer.cpp
@@ -0,0 +1,59 @@
+#include <apf.h>
+#include <apfMesh2.h>
+#include <apfNumbering.h>
+
+#include "goal_control.hpp"
+#include "goal_discretization.hpp"
+#include "goal_field.hpp"
+#include "goal_indexer.hpp"
+
+namespace goal {
+
+using Teuchos::rcp;
+
+Indexer::Indexer(Discretization* d, std::vector<Field*> const& f) {
+  disc = d;
+  fields = f;
+  GOAL_ALWAYS_ASSERT(fields.size() > 0);
+  for (size_t i = 0; i < fields.size(); ++i)
+    GOAL_ALWAYS_ASSERT(fields[i]->get_associated_dof_idx() == (int)i);
+  comm = Tpetra::DefaultPlatform::getDefaultPlatform().getComm();
+}
+
+Indexer::~Indexer() {
+  comm = Teuchos::null;
+  owned_map = Teuchos::null;
+  ghost_map = Teuchos::null;
+  node_map = Teuchos::null;
+  coords = Teuchos::null;
+  owned_graph = Teuchos::null;
+  ghost_graph = Teuchos::null;
+}
+
+apf::Mesh* Indexer::get_apf_mesh() {
+  return disc->get_apf_mesh();
+}
+
+int Indexer::get_num_fields() const {
+  return fields.size();
+}
+
+int Indexer::get_num_total_dofs(const int t) const {
+  int dofs = 0;
+  for (int f = 0; f < get_num_fields(); ++f)
+    dofs += fields[f]->get_num_nodes(t);
+  return dofs;
+}
+
+int Indexer::get_num_node_sets() const {
+  return disc->get_num_node_sets();
+}
+
+std::vector<apf::Node> const& Indexer::get_node_set_nodes(
+    std::string const& set, const int i) {
+  if (! node_sets.count(set))
+    fail("node set %s not found", set.c_str());
+  return node_sets[set][i];
+}
+
+} // end namespace goal
---
+++ src/goal_indexer.hpp
@@ -0,0 +1,152 @@
+#ifndef goal_indexer_hpp
+#define goal_indexer_hpp
+
+/// @file goal_indexer.hpp
+
+#include "goal_data_types.hpp"
+
+/// @cond
+namespace apf {
+struct Node;
+class Mesh;
+class Field;
+class FieldShape;
+class MeshEntity;
+template <class T> class NumberingOf;
+typedef NumberingOf<int> Numbering;
+typedef NumberingOf<long> GlobalNumbering;
+}
+/// @endcond
+
+namespace goal {
+
+/// @cond
+class Field;
+class Discretization;
+/// @endcond
+
+/// @brief The type of indexer.
+enum IndexerType {
+  /// @brief A strided indexer type.
+  STRIDED = 0
+};
+
+/// @brief The DOF indexer interface.
+/// @details The indexer structure is responsible for building the maps and
+/// graphs that describe the parallel distribution of local to global data
+/// in the linear algebra data structures. It does this by numbering the
+/// DOFs associated with an input vector of DOF fields. This class is also
+/// responsible for building node sets that are associated with the
+/// specification of Dirichlet boundary conditions. This class is an
+/// abstract base class, from which specific DOF indexing strategies
+/// can be implemented.
+///
+/// See the Tpetra tutorial \cite TpetraTutorial for more details.
+class Indexer {
+
+  public:
+
+    /// @brief Construct the indexer.
+    /// @param d The relevant \ref goal::Discretization.
+    /// @param f A vector of \ref goal::Field s to index.
+    Indexer(Discretization* d, std::vector<Field*> const& f);
+
+    /// @brief Destroy the indexer.
+    /// @details This destroys maps, graphs, and multi-vectors that the
+    /// indexer has constructed.
+    ~Indexer();
+
+    /// @brief Get the owned DOF map.
+    RCP<const Map> get_owned_map() { return owned_map; }
+
+    /// @brief Get the ghost DOF map.
+    RCP<const Map> get_ghost_map() { return ghost_map; }
+
+    /// @brief Get the owned CRS element connectivity graph.
+    RCP<const Graph> get_owned_graph() { return owned_graph; }
+
+    /// @brief Get the ghost CRS element connectivity graph.
+    RCP<const Graph> get_ghost_graph() { return ghost_graph; }
+
+    /// @brief Get the coord multivector for owned nodes.
+    /// @details Only non-null if all input fields have same basis type.
+    RCP<MultiVector> get_coords() { return coords; }
+
+    /// @brief Get the ith \ref goal::Field.
+    Field* get_field(const int i) { return fields[i]; }
+
+    /// @brief Get the underlying \ref goal::Discretization.
+    Discretization* get_disc() { return disc; }
+
+    /// @brief Get the underlying APF mesh.
+    apf::Mesh* get_apf_mesh();
+
+    /// @brief Get the total number of fields.
+    int get_num_fields() const;
+
+    /// @brief Get the total number of DOFs on this entity type.
+    int get_num_total_dofs(const int t) const;
+
+    /// @brief Get the number of node sets.
+    int get_num_node_sets() const;
+
+    /// @brief Get the nodes in a given node set.
+    /// @param set The relevant node set name.
+    /// @param i The relevant \ref goal::Field index.
+    std::vector<apf::Node> const& get_node_set_nodes(
+        std::string const& set, const int i);
+
+    /// @brief Get the offset into elemental DOFs.
+    /// @param i The \ref goal::Field index.
+    /// @param n The elemental node associated with the field.
+    virtual int get_elem_dof_offset(const int i, const int n) = 0;
+
+    /// @brief Get the ghost local row ID.
+    /// @param i The \ref goal::Field index.
+    /// @param e The relevant mesh entity.
+    /// @param n The local node associated with the mesh entity.
+    virtual LO get_ghost_lid(
+        const int i, apf::MeshEntity* e, const int n) = 0;
+
+    /// @brief Get the owned local row ID.
+    /// @param i The \ref goal::Field index.
+    /// @param n An APF node data structure.
+    virtual LO get_owned_lid(const int i, apf::Node const& n) = 0;
+
+    /// @brief Get entity ghost local row IDs.
+    /// @param e The relevant mesh entity.
+    /// @param lids The returned local IDs.
+    virtual void get_ghost_lids(
+        apf::MeshEntity* e, std::vector<LO>& lids) = 0;
+
+    /// @brief Sum values from the vector du into the fields f.
+    /// @param f The \ref goal::Field s to sum into.
+    /// @param du The relevant increment vector.
+    virtual void add_to_fields(
+        std::vector<Field*> const& f, RCP<Vector> du) = 0;
+
+    /// @brief Set values from the vector x to the fields f.
+    /// @param f The \ref goal::Field s to fill in.
+    /// @param x The relevant vector to grab data from.
+    virtual void set_to_fields(
+        std::vector<Field*> const& f, RCP<Vector> x) = 0;
+
+  private:
+
+    Discretization* disc;
+    std::vector<Field*> fields;
+    std::map<std::string,
+      std::vector<std::vector<apf::Node> > > node_sets;
+
+    RCP<const Comm> comm;
+    RCP<const Map> owned_map;
+    RCP<const Map> ghost_map;
+    RCP<const Map> node_map;
+    RCP<MultiVector> coords;
+    RCP<Graph> owned_graph;
+    RCP<Graph> ghost_graph;
+};
+
+} // end namespace goal
+
+#endif
