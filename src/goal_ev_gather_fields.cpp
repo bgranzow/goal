88aece2769fb5682eacec6fe944f256b6b813f13
Brian Granzow <brian.granzow@gmail.com>
2017-05-16 15:38:15 -0400

    gather fields

---
+++ src/goal_ev_gather_fields.cpp
@@ -0,0 +1,99 @@
+#include <apf.h>
+
+#include "goal_control.hpp"
+#include "goal_ev_gather_fields.hpp"
+#include "goal_field.hpp"
+#include "goal_indexer.hpp"
+#include "goal_workset.hpp"
+
+namespace goal {
+
+template <typename TRAITS>
+GatherFields<goal::Traits::Residual, TRAITS>::GatherFields(
+    Indexer* i, std::vector<Field*> const& f, int type)
+    : indexer(i),
+      fields(f) {
+  num_fields = fields.size();
+  num_nodes = fields[0]->get_num_nodes(type);
+  u.resize(num_fields);
+  for (int i = 0; i < num_fields; ++i) {
+    u[i] = PHX::MDField<ScalarT, Ent, Node>(f[i]->name(), f[i]->dl(type));
+  }
+  this->setName("Gather Fields");
+}
+
+template <typename TRAITS>
+void GatherFields<goal::Traits::Residual, TRAITS>::postRegistrationSetup(
+    SetupData d, PHX::FieldManager<TRAITS>& fm) {
+  for (int i = 0; i < num_fields; ++i)
+    this->utils.setFieldData(u[i], fm);
+  (void)d;
+}
+
+template <typename TRAITS>
+void GatherFields<goal::Traits::Residual, TRAITS>::evaluateFields(
+    EvalData workset) {
+  apf::NewArray<double> values;
+  for (int elem = 0; elem < workset.size; ++elem) {
+    auto e = workset.entities[elem];
+    for (int field = 0; field < num_fields; ++field) {
+      auto f = fields[field];
+      auto apf_f = f->get_apf_field();
+      auto fe = apf::createElement(apf_f, e);
+      apf::getScalarNodes(fe, values);
+      for (int n = 0; n < num_nodes; ++n)
+        u[field](elem, n) = values[n];
+      apf::destroyElement(fe);
+    }
+  }
+}
+
+template <typename TRAITS>
+GatherFields<goal::Traits::Jacobian, TRAITS>::GatherFields(
+    Indexer* i, std::vector<Field*> const& f, int type)
+    : indexer(i),
+      fields(f) {
+  num_fields = fields.size();
+  num_nodes = fields[0]->get_num_nodes(type);
+  u.resize(num_fields);
+  for (int i = 0; i < num_fields; ++i) {
+    u[i] = PHX::MDField<ScalarT, Ent, Node>(f[i]->name(), f[i]->dl(type));
+  }
+  this->setName("Gather Fields");
+}
+
+template <typename TRAITS>
+void GatherFields<goal::Traits::Jacobian, TRAITS>::postRegistrationSetup(
+    SetupData d, PHX::FieldManager<TRAITS>& fm) {
+  for (int i = 0; i < num_fields; ++i)
+    this->utils.setFieldData(u[i], fm);
+  (void)d;
+}
+
+template <typename TRAITS>
+void GatherFields<goal::Traits::Jacobian, TRAITS>::evaluateFields(
+    EvalData workset) {
+  apf::NewArray<double> values;
+  for (int elem = 0; elem < workset.size; ++elem) {
+    auto e = workset.entities[elem];
+    for (int field = 0; field < num_fields; ++field) {
+      auto f = fields[field];
+      auto apf_f = f->get_apf_field();
+      auto seed = f->get_seed_value();
+      auto idx = f->get_associated_dof_idx();
+      auto fe = apf::createElement(apf_f, e);
+      apf::getScalarNodes(fe, values);
+      for (int n = 0; n < num_nodes; ++n) {
+        u[field](elem, n).val() = values[n];
+        auto offset = indexer->get_elem_dof_offset(idx, n);
+        u[field](elem, n).fastAccessDx(offset) = seed;
+      }
+      apf::destroyElement(fe);
+    }
+  }
+}
+
+template class GatherFields<goal::Traits::Residual, goal::Traits>;
+template class GatherFields<goal::Traits::Jacobian, goal::Traits>;
+
+} // end namespace goal
---
+++ src/goal_ev_gather_fields.hpp
@@ -0,0 +1,100 @@
+#ifndef goal_ev_gather_fields_hpp
+#define goal_ev_gather_fields_hpp
+
+/// @file goal_ev_gather_fields_hpp
+
+#include <Phalanx_Evaluator_Derived.hpp>
+#include <Phalanx_Evaluator_WithBaseImpl.hpp>
+
+#include "goal_dimension.hpp"
+#include "goal_traits.hpp"
+
+namespace goal {
+
+/// @cond
+class Field;
+class Indexer;
+template <typename EVALT, typename TRAITS> class GatherFields;
+/// @endcond
+
+/// @brief Gather from APF fields, Residual specialization.
+template <typename TRAITS>
+class GatherFields<goal::Traits::Residual, TRAITS>
+    : public PHX::EvaluatorWithBaseImpl<TRAITS>,
+      public PHX::EvaluatorDerived<goal::Traits::Residual, TRAITS> {
+
+  public:
+
+    /// @cond
+    using SetupData = typename TRAITS::SetupData;
+    using PreEvalData = typename TRAITS::PreEvalData;
+    using PostEvalData = typename TRAITS::PostEvalData;
+    using EvalData = typename TRAITS::EvalData;
+    using ScalarT = typename goal::Traits::Residual::ScalarT;
+    /// @endcond
+
+    /// @brief Construct the evaluator.
+    /// @param i The relevant \ref goal::Indexer.
+    /// @param f The \ref goal::Field s to gather data from.
+    GatherFields(Indexer* i, std::vector<Field*> const& f, int type);
+
+    /// @brief Finalize the field manager registration.
+    /// @param d The setup data (void*).
+    /// @param fm The phalanx field manager to register.
+    void postRegistrationSetup(SetupData d, PHX::FieldManager<TRAITS>& fm);
+
+    /// @brief Fill in the local multi-dimensional arrays.
+    /// @param workset The workset to operate over.
+    void evaluateFields(EvalData workset);
+
+  private:
+
+    int num_fields;
+    int num_nodes;
+    Indexer* indexer;
+    std::vector<Field*> fields;
+    std::vector<PHX::MDField<ScalarT, Ent, Node> > u;
+};
+
+/// @brief Gather from APF fields, Jacobian specialization.
+template <typename TRAITS>
+class GatherFields<goal::Traits::Jacobian, TRAITS>
+    : public PHX::EvaluatorWithBaseImpl<TRAITS>,
+      public PHX::EvaluatorDerived<goal::Traits::Jacobian, TRAITS> {
+
+  public:
+
+    /// @cond
+    using SetupData = typename TRAITS::SetupData;
+    using PreEvalData = typename TRAITS::PreEvalData;
+    using PostEvalData = typename TRAITS::PostEvalData;
+    using EvalData = typename TRAITS::EvalData;
+    using ScalarT = typename goal::Traits::Jacobian::ScalarT;
+    /// @endcond
+
+    /// @brief Construct the evaluator.
+    /// @param i The relevant \ref goal::Indexer.
+    /// @param f The \ref goal::Field s to gather data from.
+    GatherFields(Indexer* i, std::vector<Field*> const& f, int type);
+
+    /// @brief Finalize the field manager registration.
+    /// @param d The setup data (void*).
+    /// @param fm The phalanx field manager to register.
+    void postRegistrationSetup(SetupData d, PHX::FieldManager<TRAITS>& fm);
+
+    /// @brief Fill in the local multi-dimensional arrays.
+    /// @param workset The workset to operate over.
+    void evaluateFields(EvalData workset);
+
+  private:
+
+    int num_fields;
+    int num_nodes;
+    Indexer* indexer;
+    std::vector<Field*> fields;
+    std::vector<PHX::MDField<ScalarT, Ent, Node> > u;
+};
+
+} // end namespace goal
+
+#endif
