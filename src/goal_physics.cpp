0dbd7321fefbf0bbd68946214f3c850334f54926
Brian Granzow <brian.granzow@gmail.com>
2017-05-16 13:56:31 -0400

    physics

---
+++ src/goal_physics.cpp
@@ -0,0 +1,105 @@
+#include "goal_control.hpp"
+#include "goal_discretization.hpp"
+#include "goal_field.hpp"
+#include "goal_indexer.hpp"
+#include "goal_physics.hpp"
+
+namespace goal {
+
+using Teuchos::rcp;
+
+Physics::Physics(Discretization* d) {
+  disc = d;
+}
+
+void Physics::build_coarse_indexer() {
+  indexer = create_indexer(disc, u);
+}
+
+void Physics::build_fine_indexer() {
+  indexer = create_indexer(disc, u_fine);
+}
+
+void Physics::build_error_indexer() {
+  indexer = create_indexer(disc, e);
+}
+
+void Physics::destroy_indexer() {
+  goal::destroy_indexer(indexer);
+}
+
+void Physics::build_primal_model() {
+  auto t0 = time();
+  auto nes = disc->get_num_elem_sets();
+  auto nss = disc->get_num_side_sets();
+  vfms.resize(nes);
+  nfms.resize(nss);
+  for (int i = 0; i < nes; ++i) {
+    elem_set = i;
+    vfms[i] = rcp(new PHX::FieldManager<goal::Traits>);
+    build_primal_volumetric(vfms[i]);
+  }
+  for (int i = 0; i < nss; ++i) {
+    side_set = i;
+    nfms[i] = rcp(new PHX::FieldManager<goal::Traits>);
+    build_primal_neumann(nfms[i]);
+  }
+  dfm = rcp(new PHX::FieldManager<goal::Traits>);
+  build_primal_dirichlet(dfm);
+  auto t1 = time();
+  print(" > primal model built in %f seconds", t1 - t0);
+}
+
+void Physics::build_dual_model() {
+  auto t0 = time();
+  auto nes = disc->get_num_elem_sets();
+  auto nss = disc->get_num_side_sets();
+  vfms.resize(nes);
+  nfms.resize(nss);
+  for (int i = 0; i < nes; ++i) {
+    elem_set = i;
+    vfms[i] = rcp(new PHX::FieldManager<goal::Traits>);
+    build_dual_volumetric(vfms[i]);
+  }
+  for (int i = 0; i < nss; ++i) {
+    side_set = i;
+    nfms[i] = rcp(new PHX::FieldManager<goal::Traits>);
+    build_dual_neumann(nfms[i]);
+  }
+  dfm = rcp(new PHX::FieldManager<goal::Traits>);
+  build_dual_dirichlet(dfm);
+  auto t1 = time();
+  print(" > dual model built in %f seconds", t1 - t0);
+}
+
+void Physics::build_error_model() {
+  auto t0 = time();
+  auto nes = disc->get_num_elem_sets();
+  auto nss = disc->get_num_side_sets();
+  vfms.resize(nes);
+  nfms.resize(nss);
+  for (int i = 0; i < nes; ++i) {
+    elem_set = i;
+    vfms[i] = rcp(new PHX::FieldManager<goal::Traits>);
+    build_error_volumetric(vfms[i]);
+  }
+  for (int i = 0; i < nss; ++i) {
+    side_set = i;
+    nfms[i] = rcp(new PHX::FieldManager<goal::Traits>);
+    build_error_neumann(nfms[i]);
+  }
+  dfm = rcp(new PHX::FieldManager<goal::Traits>);
+  build_error_dirichlet(dfm);
+  auto t1 = time();
+  print(" > error model built in %f seconds", t1 - t0);
+}
+
+void Physics::destroy_model() {
+  for (int i = 0; i < vfms.size(); ++i)
+    vfms[i] = Teuchos::null;
+  for (int i = 0; i < nfms.size(); ++i)
+    nfms[i] = Teuchos::null;
+  dfm = Teuchos::null;
+}
+
+} // end namespace goal
---
+++ src/goal_physics.hpp
@@ -0,0 +1,133 @@
+#ifndef goal_physics_hpp
+#define goal_physics_hpp
+
+/// @file goal_physics.hpp
+
+#include <Phalanx_FieldManager.hpp>
+
+#include "goal_traits.hpp"
+
+namespace goal {
+
+using Teuchos::RCP;
+using Teuchos::ArrayRCP;
+using Teuchos::ParameterList;
+
+/// @brief A field manager template on \ref goal::Traits.
+using FieldManager = RCP<PHX::FieldManager<goal::Traits> >;
+/// @brief An array of \ref goal::FieldManager s.
+using FieldManagers = ArrayRCP<FieldManager>;
+
+/// @cond
+class Field;
+class Indexer;
+class Discretization;
+/// @endcond
+
+/// @brief The abstract physics class.
+/// @details The physics class allows users to implement physics models
+/// assuming three distinct evaluation modes:
+/// - primal -> the forward physics model.
+/// - dual -> the dual/adjoint physical model.
+/// - error -> the error localization model.
+///
+/// Each mode is responsible for constructing its own appropriate
+/// Phalanx Field Managers and registering Phalanx Evaluators in these
+/// Field Managers. No two physical models may exist at the same time.
+///
+/// These models are split into three types of evaluations:
+/// - volumetric -> evaluations over the interior (elements).
+/// - neumann -> evaluations over the Neumann boundaries (sides).
+/// - dirichlet -> evaluations over the Dirichlet boundaries (nodes).
+class Physics {
+
+  public:
+
+    /// @brief Construct the physics object.
+    /// @param d The relevant \ref goal::Discretization.
+    Physics(Discretization* d);
+
+    /// @brief Destroy the physics object.
+    /// @details This does nothing.
+    virtual ~Physics();
+
+    /// @brief Get the dirichlet boundar condition parameters.
+    /// @details Each member of this parameter list should be of type:
+    /// Teuchos::Array<std::string> with entries of the form:
+    /// [ dof name, node set name, bc value].
+    virtual ParameterList const& get_dbc_params() = 0;
+
+    /// @brief Build the coarse indexer for the space V^H.
+    /// @details This is built using the DOF fields u.
+    void build_coarse_indexer();
+
+    /// @brief Build the fine indexer for the space V^h.
+    /// @details This is built using the DOF fields u_fine.
+    void build_fine_indexer();
+
+    /// @brief Build the error indexer for the partition of unity.
+    /// @details This is built using the fields e.
+    void build_error_indexer();
+
+    /// @brief Returns the current indexer.
+    Indexer* get_indexer() { return indexer; }
+
+    /// @brief Destroy the current indexer.
+    void destroy_indexer();
+
+    /// @brief Build the user-implemented primal model.
+    void build_primal_model();
+
+    /// @brief Build the user-implemented dual model.
+    void build_dual_model();
+
+    /// @brief Build the user-implemented error model.
+    void build_error_model();
+
+    /// @brief Destroy the current model.
+    void destroy_model();
+
+    /// @brief Returns the volumetric field managers.
+    FieldManagers get_volumetric() { return vfms; }
+
+    /// @brief Returns the neumann field managers.
+    FieldManagers get_neumann() { return nfms; }
+
+    /// @brief Returns the dirichlet field manager.
+    FieldManager get_dirichlet() { return dfm; }
+
+  protected:
+
+    virtual void build_primal_volumetric(FieldManager) {}
+    virtual void build_primal_neumann(FieldManager) {}
+    virtual void build_primal_dirichlet(FieldManager) {}
+
+    virtual void build_dual_volumetric(FieldManager) {}
+    virtual void build_dual_neumann(FieldManager) {}
+    virtual void build_dual_dirichlet(FieldManager) {}
+
+    virtual void build_error_volumetric(FieldManager) {}
+    virtual void build_error_neumann(FieldManager) {}
+    virtual void build_error_dirichlet(FieldManager) {}
+
+    Discretization* disc;
+    Indexer* indexer;
+
+    FieldManagers vfms;
+    FieldManagers nfms;
+    FieldManager dfm;
+
+    std::vector<Field*> u;
+    std::vector<Field*> z;
+    std::vector<Field*> e;
+
+    std::vector<Field*> u_fine;
+    std::vector<Field*> z_fine;
+
+    int elem_set;
+    int side_set;
+};
+
+} // end namespace goal
+
+#endif
