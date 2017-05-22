3f004b2f00c7ce7d7d7bdb3b58386ca29f6d0f5d
Brian Granzow <brian.granzow@gmail.com>
2017-05-22 11:47:39 -0400

    aux: add jenga config script

---
+++ aux/do-config-jenga.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -ex
+
+# Modify these paths for your system
+TRILINOS_DIR=/lore/granzb/trilinos_nightly/install
+GOAL_SRC_DIR=/lore/granzb/goal
+GOAL_INSTALL_DIR=/lore/granzb/goal/install
+
+cmake $GOAL_SRC_DIR \
+-DCMAKE_CXX_COMPILER="mpicxx" \
+-DCMAKE_INSTALL_PREFIX=$GOAL_INSTALL_DIR \
+-DBUILD_TESTING=ON \
+-DSCOREC_PREFIX=$TRILINOS_DIR \
+-DTrilinos_PREFIX=$TRILINOS_DIR \
+-DGoal_FAD_SIZE=40 \
+2>&1 | tee config_log
