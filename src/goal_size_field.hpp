6a43e3217eb9d421fac4eb6ad013c5cafabbbb2a
Brian Granzow <brian.granzow@gmail.com>
2017-10-30 13:55:47 -0400

    [size field] : untested

---
+++ src/goal_size_field.hpp
@@ -0,0 +1,14 @@
+#ifndef goal_size_field_hpp
+#define goal_size_field_hpp
+
+namespace apf {
+class Field;
+}
+
+namespace goal {
+
+apf::Field* get_iso_target_size(apf::Field* e, int target);
+
+}
+
+#endif
