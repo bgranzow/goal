51eaf1c5307848483ecd72d5e8bb06bc962546c8
Brian Granzow <brian.granzow@gmail.com>
2017-10-31 13:47:13 -0400

    [main nested] : target growth factor

--- src/main_nested.cpp
+++ src/main_nested.cpp
@@ -45,6 +45,7 @@ static ParameterList get_valid_adapt_params() {
   p.set<bool>("shoud coarsen", true);
   p.set<double>("good quality", 0.3);
   p.set<int>("target elems", 1);
+  p.set<double>("target growth", 2.0);
   return p;
 }
 
@@ -111,6 +112,8 @@ void Solver::adapt(int step, int cycle, double t_now, double t_old) {
   auto mesh = disc->get_apf_mesh();
   auto adapt_params = params->sublist("adaptation");
   auto target = adapt_params.get<int>("target elems");
+  auto scale = adapt_params.get<double>("target growth", 1.0);
+  target = target * std::pow(scale, (cycle - 1.0));
   auto size = get_iso_target_size(e, target);
   auto in = ma::configure(mesh, size);
   configure_ma(in, adapt_params);
