d17e4bba89fe559590eed2ef6f289ab987b17123
Brian Granzow <brian.granzow@gmail.com>
2018-03-14 20:30:23 -0400

    bforce : slight progress

--- src/goal_bforce.cpp
+++ src/goal_bforce.cpp
@@ -1,6 +1,9 @@
 #include "goal_control.hpp"
+#include "goal_disc.hpp"
 #include "goal_displacement.hpp"
+#include "goal_states.hpp"
 #include "goal_vector_weight.hpp"
+
 #include "goal_bforce.hpp"
 
 namespace goal {
@@ -10,9 +13,13 @@ using Teuchos::rcp_static_cast;
 template <typename T>
 BForce<T>::BForce(
   RCP<Integrator> u_,
-  RCP<Integrator> w_) :
+  RCP<Integrator> w_,
+  States* s_,
+  ParameterList const& p_) :
     u(rcp_static_cast<Displacement<T>>(u_)),
     w(rcp_static_cast<VectorWeight>(w_)),
+    states(s_),
+    params(p_),
     num_dims(u->get_num_dims()) {
   GOAL_DEBUG_ASSERT(Teuchos::nonnull(u));
   GOAL_DEBUG_ASSERT(Teuchos::nonnull(w));
@@ -20,10 +27,31 @@ BForce<T>::BForce(
 }
 
 template <typename T>
+void BForce<T>::set_elem_set(int es_idx) {
+  auto d = states->get_disc();
+  auto es_name = d->get_elem_set_name(es_idx);
+  ParameterList mat = params.sublist(es_name);
+  double E = mat.get<double>("E");
+  double nu = mat.get<double>("nu");
+  k = E / (3.0 * (1.0 - 2.0 * nu));
+  mu = E / (2.0 * (1.0 + nu));
+}
+
+template <typename T>
+void BForce<T>::in_elem(apf::MeshElement* me) {
+  elem = me;
+}
+
+template <typename T>
 void BForce<T>::at_point(apf::Vector3 const& xi, double ipw, double dv) {
   (void)xi;
   (void)ipw;
   (void)dv;
+}
+
+template <typename T>
+void BForce<T>::out_elem() {
+  elem = 0;
 }
 
 template class BForce<ST>;
--- src/goal_bforce.hpp
+++ src/goal_bforce.hpp
@@ -6,18 +6,32 @@
 namespace goal {
 
 using Teuchos::RCP;
+using Teuchos::ParameterList;
 
+class States;
 class VectorWeight;
 
 template <typename T>
 class BForce : public Integrator {
   public:
-    BForce(RCP<Integrator> u, RCP<Integrator> w);
+    BForce(
+        RCP<Integrator> u,
+        RCP<Integrator> w,
+        States* s,
+        ParameterList const& p);
+    void set_elem_set(int es_idx);
+    void in_elem(apf::MeshElement* me);
     void at_point(apf::Vector3 const& xi, double ipw, double dv);
+    void out_elem();
   private:
     RCP<Displacement<T>> u;
     RCP<VectorWeight> w;
+    States* states;
+    ParameterList params;
     int num_dims;
+    double mu;
+    double k;
+    apf::MeshElement* elem;
 };
 
 }
